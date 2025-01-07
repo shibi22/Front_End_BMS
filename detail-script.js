73640bf8fe8dc8defd7c80bd1f3943ca87e99007
shibiraj <124015659+shibi22@users.noreply.github.com>
2025-01-07 16:59:16 +0530

    Update detail-script.js

--- detail-script.js
+++ detail-script.js
@@ -11,7 +11,7 @@ window.onload = async () => {
 async function fetchMovieDetails(movieId) {
   try {
     const response = await fetch(
-      `https://learn-with-guvi-book-my-show-backend.onrender.com/movie/${movieId}`
+      `https://back-end-bms.onrender.com/movie/${movieId}`
     );
     const movie = await response.json();
 
@@ -119,7 +119,7 @@ document.getElementById("submitBooking").addEventListener("click", async () => {
 
   try {
     const response = await fetch(
-      "https://learn-with-guvi-book-my-show-backend.onrender.com/movie/book-movie",
+      "https://back-end-bms/movie/book-movie",
       {
         method: "POST",
         headers: {
